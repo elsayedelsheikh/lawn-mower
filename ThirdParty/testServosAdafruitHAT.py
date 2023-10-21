5bf59c9e412747a646abf14c703542bc56071f64
elsayedelsheikh <mec.sayed97@gmail.com>
2023-10-21 15:55:33 +0300

    Update

--- ThirdParty/testServosAdafruitHAT.py
+++ ThirdParty/testServosAdafruitHAT.py
@@ -2,12 +2,34 @@ import time
 from adafruit_servokit import ServoKit
 kit = ServoKit(channels=16)
 
-kit.servo[0].actuation_range = 160
+## Backleft 
+kit.servo[2].actuation_range = 160
+
+## Backright
+kit.servo[6].actuation_range = 160
+
+## Frontleft
+kit.servo[10].actuation_range = 160
+
+## Frontright
+kit.servo[14].actuation_range = 160
 
 while True:
-	kit.servo[0] = 0
+	kit.servo[2] = 0
+	kit.servo[6] = 0
+	kit.servo[10] = 0
+	kit.servo[14] = 0
 	time.sleep(3)
-	kit.servo[0] = 90
+
+	kit.servo[2] = 90
+	kit.servo[6] = 90
+	kit.servo[10] = 90
+	kit.servo[14] = 90
 	time.sleep(3)
-	kit.servo[0] = 160
+
+	kit.servo[2] = 160
+	kit.servo[6] = 160
+	kit.servo[10] = 160
+	kit.servo[14] = 160
+	time.sleep(3)
 	
