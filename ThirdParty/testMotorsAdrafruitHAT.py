7cc61561957d0a142328e6f2ba7dbff588ee1cd2
elsayedelsheikh <mec.sayed97@gmail.com>
2023-10-21 16:29:49 +0300

    ...

--- ThirdParty/testMotorsAdrafruitHAT.py
+++ ThirdParty/testMotorsAdrafruitHAT.py
@@ -3,7 +3,22 @@ import busio
 import adafruit_pca9685
 i2c = busio.I2C(board.SCL, board.SDA)
 hat = adafruit_pca9685.PCA9685(i2c)
-
 hat.frequency = 60
-motor_ch = hat.channels[0]
-motor_ch.duty_cycle = 0xffff
+
+## Backleft
+motor_bl = hat.channels[0]
+
+## Backright
+motor_br = hat.channels[4]
+
+## Frontleft
+motor_fl = hat.channels[8]
+
+## Frontright
+motor_fr = hat.channels[12]
+
+## Set the duty cycle to 0xffff
+motor_bl.duty_cycle = 0xffff
+motor_br.duty_cycle = 0xffff
+motor_fl.duty_cycle = 0xffff
+motor_fr.duty_cycle = 0xffff
